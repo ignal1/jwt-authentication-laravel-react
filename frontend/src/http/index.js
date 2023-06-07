460689077640a455d6f83326822f58e6a7ba167a
ignal1 <alexeix@mail.ua>
2023-06-07 19:17:47 +0300

    changed frontend

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import store from "../store";
 
-export const API_URL = 'http://localhost:8080/api'
+export const API_URL = 'http://localhost:8000/api'
 
 const api = axios.create({
   baseURL: API_URL
@@ -16,7 +16,8 @@ api.interceptors.response.use(config => {
   return config
 }, async error => {
   const originalRequest = error.config
-  if(error.status === 401){
+  if(error.response.status === 401 && error.config && !error.config._isRetry){
+    originalRequest._isRetry = true
     try{
       await store.refresh()
       return api.request(originalRequest)
@@ -24,6 +25,7 @@ api.interceptors.response.use(config => {
       console.log('Не авторизован')
     }
   }
+  throw error
 })
 
 export default api;
